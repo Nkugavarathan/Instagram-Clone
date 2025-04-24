e9a6e60eb2fbf8ff5f828f7f7fb228cc5c225e93
Nkugavarathan <kugavarathan28@gmail.com>
2025-04-24 13:00:02 +0530

    suggestionfinished

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -1,5 +1,69 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 export default function Suggestion() {
-  return <div>Suggestion</div>
+  const [profile, setProfile] = useState()
+  const [suggestion, setSuggestion] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/profile")
+      .then((data) => data.json())
+      .then((profile) => setProfile(profile))
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/suggestions")
+      .then((data) => data.json())
+      .then((suggest) => setSuggestion(suggest))
+  }, [])
+  return (
+    <div className="m-3 ">
+      <div>
+        {profile ? (
+          <div className="d-flex align-items-center ">
+            <img
+              src={profile.profilePic}
+              alt="Profile"
+              className="rounded-circle me-3"
+              style={{ width: "45px", height: "45px", objectFit: "cover" }}
+            />
+            <h5 className="mb-0 ">{profile.username}</h5>
+            <p className="ms-auto text-primary" style={{ cursor: "pointer" }}>
+              Switch
+            </p>
+          </div>
+        ) : (
+          <div>Loading</div>
+        )}
+      </div>
+      <div className=" d-flex align-items-center my-2 text-dark">
+        <p>Suggested for You</p>
+        <b className="ms-auto " style={{ cursor: "pointer" }}>
+          See All
+        </b>
+      </div>
+      <div className="suggestion">
+        {suggestion.length > 0 ? (
+          suggestion.map((sug) => (
+            <div
+              className="d-flex align-items-center my-2 bg-transparent "
+              key={sug.id}
+            >
+              <img
+                src={sug.profilePic}
+                alt="Profile"
+                className="rounded-circle me-3"
+                style={{ width: "45px", height: "45px", objectFit: "cover" }}
+              />
+              <h5 className="mb-0">{sug.username}</h5>
+              <p className="ms-auto text-primary" style={{ cursor: "pointer" }}>
+                Follow
+              </p>
+            </div>
+          ))
+        ) : (
+          <div>Loading</div>
+        )}
+      </div>
+    </div>
+  )
 }
